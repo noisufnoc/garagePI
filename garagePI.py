b65266db21f9d61ff8d2c0d552ae9dc4adaca438
Mike Walker <noisufnoc@gmail.com>
2012-11-25 19:44:53 -0800

    cleanup cleanup

--- garagePI.py
+++ garagePI.py
@@ -8,25 +8,30 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 
 @app.route('/')
-def api_root():
+def garage_root():
     return 'I am working!'
 
 @app.route('/true')
-def api_true():
+def garage_true():
     GPIO.output(18, True)
     return 'True'
 
 @app.route('/false')
-def api_false():
+def garage_false():
     GPIO.output(18, False)
     return 'False'
 
 @app.route('/open')
-def api_open():
+def garage_open():
     #close circuit
     #wait 2 seconds
     #open circuit
     return 'Open'
+
+@app.route('/cleanup')
+def garage_cleanup():
+    GPIO.cleanup()
+    return 'All clean'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
